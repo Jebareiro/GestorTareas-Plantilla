2e386b6886bc08eed3071763d1386bc91ce43b1c
Jebareiro <jessibareiroaponte@gmail.com>
2024-11-18 20:12:07 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,43 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/HomeView.vue';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AddTask from '@/views/AddTask.vue';
+import CombinedView from '@/views/CombinedView.vue';
+import TaskList from '@/views/TaskList.vue';
+
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
-  }
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue'),
+  },
+  {
+    path: '/addtask',
+    name: 'AddTask',
+    component: AddTask
+  },
+  {
+    path: '/combined',
+    name: 'CombinedView',
+    component: CombinedView
+  },
+  {
+    path: '/tasklist',
+    name: 'TaskList',
+    component: TaskList
+  },
 ];
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 });
 
